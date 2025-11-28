761eb93e9e103724fb9c19873dca4963820f0308
ArpanCharola <arpancharola11@gmail.com>
2025-11-28 18:34:16 +0530

    Concatenation of array leetcode easy

---
+++ Arrays/concatenateArray.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+public class concatenateArray {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+        for(int i = 0; i < n; i++){
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        concatenateArray c = new concatenateArray();
+        int[] res = c.getConcatenation(nums);
+        System.out.println(Arrays.toString(res));
+    }
+}
