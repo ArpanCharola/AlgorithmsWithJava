97b25ec45b876e16a896d3253631d9ff4b714721
ArpanCharola <arpancharola11@gmail.com>
2025-12-04 22:25:06 +0530

    Smallest Letter Update

--- Leetcode/TwoSum.java
+++ Leetcode/TwoSum.java
@@ -6,8 +6,6 @@ import java.util.Arrays;
 class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
-        
-        int sum = 0;
         for(int i = 0; i < n; i++){
             for(int j = i + 1; j <n; j++){
                 if(nums[i] + nums[j] == target){
